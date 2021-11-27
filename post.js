c6e237243540c48f3b0a85b09d6457caf7196e1e
exl7069 <exl7069@g.rit.edu>
2021-11-26 21:50:38 -0500

    frontend design project

---
+++ post.js
@@ -0,0 +1,9 @@
+function post_post(){
+
+	var btn = document.getElementById("btn");
+	var txt_area = document.getElementById("txt");
+	alert("Successfully post a new post");
+	window.location.href= "forum_after_post.html";
+	
+	
+}
